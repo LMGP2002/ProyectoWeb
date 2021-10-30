9248f6b74e8727f0e0e6e87f02c15062d702bdc7
LMGP2002 <luiguerrero@uniboyaca.edu.co>
2021-10-30 15:39:34 -0500

    Error scroll corregido

--- JS/estilos.js
+++ JS/estilos.js
@@ -1,6 +1,7 @@
 window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
+    window.scrollTo(0,0);
 }
+
 document.addEventListener('scroll',()=>{
     let nav=document.querySelector('.nav');
     console.log(nav);
@@ -10,3 +11,18 @@ document.addEventListener('scroll',()=>{
         nav.style.backgroundColor='#222';
     }else nav.style.backgroundColor='transparent';
 });
+
+// código para el Smooth Scroll
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 340, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
