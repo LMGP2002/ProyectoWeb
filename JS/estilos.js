1a3fed88d421150fcc40443f30b7575e531bb00a
LMGP2002 <luiguerrero@uniboyaca.edu.co>
2021-10-31 12:36:18 -0500

    Velocidad de scroll

--- JS/estilos.js
+++ JS/estilos.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
         var hash = this.hash;
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 340, function(){
+        }, 330, function(){
           window.location.hash = hash;
         });
       } 
