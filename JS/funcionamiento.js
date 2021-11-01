ce38c4a0795a09ac48bcafa3b1608c188c579e90
01LauraSofia03 <laura99diaz99rodriguez@gmail.com>
2021-11-01 14:36:09 -0500

    Validación solo números

--- JS/funcionamiento.js
+++ JS/funcionamiento.js
@@ -6,3 +6,34 @@ function imc(){
     document.getElementById('res').value=`Su índice de masa corporal es ${resultado} `;
   
 }
+function meta(){
+    var sex = document.getElementById("sex").value;
+    var pes = document.getElementById("pes").value;
+    var esta = document.getElementById("esta").value;
+    var ed = document.getElementById("ed").value;
+    var r = (10*pes)+(6.25*esta)-(5*ed);
+    if(sex === "mujer"){
+        var to = r - 161;
+    }else{
+        var to = r + 5;
+    }
+    document.getElementById('resmeta').value=`Su metabolismo basal es ${to} `;
+  
+}
+function soloNumeros(e){
+    key=e.keycode || e.which;
+    teclado=String.fromCharCode(key);
+    numeros="0123456789";
+    especiales=[8,37,38,46];
+    tecladoEspecial=false;
+    for(item of especiales){
+        if(key==item){
+            if(teclado!='%'&&teclado!='&'&&teclado!='.'){
+                tecladoEspecial=true;
+            }
+        }
+    }
+    if(numeros.indexOf(teclado)==-1 &&!tecladoEspecial){
+        return false;
+    } 
+}
