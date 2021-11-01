8363881bca2421329f4530dba568f889ced59860
LMGP2002 <luiguerrero@uniboyaca.edu.co>
2021-10-31 19:08:25 -0500

    modificación del about us

--- JS/estilos.js
+++ JS/estilos.js
@@ -4,9 +4,7 @@ window.onbeforeunload = function () {
 
 document.addEventListener('scroll',()=>{
     let nav=document.querySelector('.nav');
-    console.log(nav);
     let scroll_position=window.scrollY;
-    console.log(scroll_position);
     if(scroll_position>140){
         nav.style.backgroundColor='#222';
     }else nav.style.backgroundColor='transparent';
