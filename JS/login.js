8ef28e8b27e095cfdd430c26557e23f3922563e1
LMGP2002 <luiguerrero@uniboyaca.edu.co>
2021-11-05 23:46:01 -0500

    Animación Sección de login

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+const $loginBtn = document.querySelector('#login_btn');
+const $signUp = document.querySelector('#signUp_btn');
+const $container = document.querySelector('.container');
+
+$signUp.addEventListener('click',()=>{
+    $container.classList.add('signUp_mode');
+});
+$loginBtn.addEventListener('click',()=>{
+    $container.classList.remove('signUp_mode');
+});
